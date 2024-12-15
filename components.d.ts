9de66a512f7729efa625aab0c7f83ed43e747344
wuvs2019819119 <wuvs2019819119@outlook.com>
2024-12-16 00:50:34 +0800

    init

--- components.d.ts
+++ components.d.ts
@@ -7,9 +7,12 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ContextMenu: typeof import('primevue/contextmenu')['default']
     FileDialog: typeof import('./src/components/FileDialog.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Row: typeof import('primevue/row')['default']
+    Select: typeof import('primevue/select')['default']
   }
 }
