1ffba3bcf0ed361fa94b8aaa2fbe3341391a8404
wzm <>
2024-12-17 18:55:36 +0800

    文件管理器前进后退

--- components.d.ts
+++ components.d.ts
@@ -12,7 +12,6 @@ declare module 'vue' {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    Row: typeof import('primevue/row')['default']
     Select: typeof import('primevue/select')['default']
   }
 }
